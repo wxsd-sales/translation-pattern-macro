5f5925a01763a2f19fd02e17398b8e20b799b75d
William Mills <wmpmills@gmail.com>
2023-04-25 18:12:23 +0200

    Update translation-pattern.js

--- translation-pattern.js
+++ translation-pattern.js
@@ -1,15 +1,4 @@
 /********************************************************
-Copyright (c) 2023 Cisco and/or its affiliates.
-This software is licensed to you under the terms of the Cisco Sample
-Code License, Version 1.1 (the "License"). You may obtain a copy of the
-License at https://developer.cisco.com/docs/licenses
-All use of the material herein must be in accordance with the terms of
-the License. All rights not expressly granted by the License are
-reserved. Unless required by applicable law or agreed to separately in
-writing, software distributed under the License is distributed on an "AS
-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
-or implied.
-*********************************************************
  * 
  * Macro Author:      	William Mills
  *                    	Technical Solutions Specialist 
@@ -27,7 +16,7 @@ or implied.
  * - Match and prefix a dialled number
  * - Ignore specific numbers
  * 
- * Full Readme and source code available on Github:
+ * Full Readme, source code and license agreement available on Github:
  * https://github.com/wxsd-sales/translation-pattern-macro
  * 
  ********************************************************/
