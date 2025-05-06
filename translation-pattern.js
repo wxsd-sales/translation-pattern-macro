40e5a0dda8325d528719924c534f7ac5187768a7
William Mills <wmpmills@gmail.com>
2025-05-06 13:32:28 +0100

    added replace text action

--- translation-pattern.js
+++ translation-pattern.js
@@ -5,8 +5,8 @@
  *                    	wimills@cisco.com
  *                    	Cisco Systems
  * 
- * Version: 1-0-0
- * Released: 15/02/23
+ * Version: 2-0-0
+ * Released: 03/05/25
  * 
  * This Webex Devices macro lets you easily define locally managed
  * translations patterns which change any dialled destination into another.
@@ -14,6 +14,7 @@
  * Features:
  * - Match and redirect a dialled number
  * - Match and prefix a dialled number
+ * - Match and replace sub regex expression with new text
  * - Ignore specific numbers
  * 
  * Full Readme, source code and license agreement available on Github:
@@ -29,6 +30,7 @@ import xapi from 'xapi';
 const patterns = [
   { regex: '^([1][2][3])$', action: 'redirect', number: 'example@webex.com' },  // Matches 123 -> redirects to example@webex.com
   { regex: '123@connect\.example\.com', action: 'redirect', number: 'example@webex.com' },  // Matches 123@connect.example.com -> redirects to example@webex.com
+  { regex: '^(.*)@zoomcrc.com$', action: 'replace', replaceRegex:'@zoomcrc.com$', replaceText: '...1234@zoomus.com' },  // Matches 123...@zoomcrc.com -> redirects to 123...@zoomus.com
   { regex: '^([7][8][9])$', action: 'redirect', number: 'example@webex.com' },  // Matches 789 -> redirects to example@webex.com
   { regex: '^([0-9]{9,12})$', action: 'append', number: '.example@webex.com' }, // Matches 9-12 digits -> <dialled> + '.example@webex.com'
   { regex: '^([0-9]{13})$', action: 'prefix', number: '9' }, // Matches 13 digits -> '9' + <dialled>
@@ -75,6 +77,11 @@ function handleOutgoing(call) {
       case 'append':
         console.log(`Number matched with expression [${match.regex}] | Redirecting to: [${callback + match.number}]`);
         changeCall(callId, callback + match.number)
+        break;
+      case 'replace':
+        const newNumber = callback.replace(new RegExp(match.replaceRegex), match.replaceText)
+        console.log(`Number matched with expression [${match.regex}] | Redirecting to : [${newNumber}]`);
+        changeCall(callId, newNumber)
         break;
       case 'continue':
         console.log(`Number matched with expression [${match.regex}] | Ignoring call`);
